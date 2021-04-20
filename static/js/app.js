307eaa0b7d27df5a46faa26b541d8640f7589b96
Ciera Morris <cieranmorris@gmail.com>
2021-04-20 16:02:46 -0600

    setting up functions for creating updated graphs

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,26 @@
 //This starter code is pulled from the office hours discussion with Dom
 console.log("app.js is loaded");
 
+//Create stubs for bar graph, bubble chart, and demographic metadata
+function drawBargraph(sampleId) {
+    console.log(`drawBargraph(${sampleId})`);
+}
+
+function drawBubblechart(sampleId) {
+    console.log(`drawBubblechart(${sampleId})`);
+}
+
+function showMetadata(sampleId) {
+    console.log(`showMetadata (${sampleId})`);
+}
+
+function optionChanged(newsampleId) {
+    console.log(`User selected ${newsampleId}`);
+
+    drawBargraph(newsampleId);
+    drawBubblechart(newsampleId);
+    showMetadata(newsampleId);
+}
 
 
 function InitDashboard() {
@@ -11,7 +31,7 @@ function InitDashboard() {
 
     //read data from samples.json using D3
     d3.json("data/samples.json").then(data => {
-        console.log(data);
+        // console.log(data);
 
         var sampleNames = data.names;
 
@@ -20,6 +40,13 @@ function InitDashboard() {
                 .text(sampleId)
                 .property("value", sampleId);
         });
+
+        
+        var id = sampleNames[0];
+
+        drawBargraph(id);
+        drawBubblechart(id);
+        showMetadata(id);
     });
 
 
