1be149d8e8e3658f8bf74f1bc37d130bbdd285a1
Ciera Morris <cieranmorris@gmail.com>
2021-04-20 16:24:03 -0600

    finished initial staging of code

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,42 @@ console.log("app.js is loaded");
 //Create stubs for bar graph, bubble chart, and demographic metadata
 function drawBargraph(sampleId) {
     console.log(`drawBargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        //console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        // console.log(result);
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        // console.log(otu_ids);
+
+        var sample_values = result.sample_values;
+        // console.log(sample_values);
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otu_Id}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    });
 }
 
 function drawBubblechart(sampleId) {
@@ -17,6 +53,7 @@ function showMetadata(sampleId) {
 function optionChanged(newsampleId) {
     console.log(`User selected ${newsampleId}`);
 
+    //draw new charts when a new Sample ID is selected in dropdown menu
     drawBargraph(newsampleId);
     drawBubblechart(newsampleId);
     showMetadata(newsampleId);
@@ -44,22 +81,11 @@ function InitDashboard() {
         
         var id = sampleNames[0];
 
+        //Draw graphs and metadata
         drawBargraph(id);
         drawBubblechart(id);
         showMetadata(id);
     });
 
-
-
-
-
-    //update bar graph
-
-
-    //update bubble chart
-
-
-    // update demographic information
 }
-
 InitDashboard();
